be71b2aa3ebf298e06f9de05503bc9c49dccdd7e
Kemal Yavuz <mkemalyavuzm@gmail.com>
2023-09-06 07:34:46 +0300

    v2

--- src/AtmProject.java
+++ src/AtmProject.java
@@ -2,5 +2,7 @@ public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Bu bir Atm projesidir");
         //versiyon1
+        System.out.println("java is easy");
+        //version2
     }
 }
